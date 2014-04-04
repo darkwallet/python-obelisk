7f6fbf96a466940dd5ec91c0b0ddb7d104aee91e
Andres Vargas <andres.vargas@interalia.net>
2014-04-04 13:33:15 -0600

    run testnet enviroment

--- obelisk/config.py
+++ obelisk/config.py
@@ -1,3 +1,5 @@
+import os
+
 class ChainParameters(object):
     def __init__(self, magic_bytes, pubkey_version, script_version, wif_version, protocol_port):
         self.magic_bytes = magic_bytes
@@ -9,7 +11,7 @@ class ChainParameters(object):
 testnet_chain = ChainParameters(0x0709110b, 0x6F, 0xC4, 0xEF, 18333)
 mainnet_chain = ChainParameters(0xd9b4bef9, 0x00, 0x05, 0x80, 8333)
 
-ENABLE_TESTNET = False
+ENABLE_TESTNET = False if not os.environ.get("ENABLE_TESTNET") else True
 
 if ENABLE_TESTNET:
     chain = testnet_chain
