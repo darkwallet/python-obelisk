5830b205d15397f3b18c9cf9472110399a665a43
Amir Taaki <genjix@riseup.net>
2015-01-26 20:25:11 +0100

    added start()/stop() convenience functions.

--- obelisk/__init__.py
+++ obelisk/__init__.py
@@ -4,7 +4,7 @@ from client import *
 from models import *
 from transaction import *
 from zmqbase import MAX_UINT32
-
+from twisted.internet import reactor
 
 def select_network(network):
     import config
@@ -12,3 +12,9 @@ def select_network(network):
         config.chain = config.testnet_chain
     else:
         config.chain = config.mainnet_chain
+
+def start():
+    reactor.run()
+def stop():
+    reactor.stop()
+
